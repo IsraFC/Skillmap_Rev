57b5b64fa0dddd3865ae33ae336b613c245493ff
IsraFC <126487666+IsraFC@users.noreply.github.com>
2025-03-28 16:45:52 -0600

    1° Commit - Codigo 28/03/2025

---
+++ Skillmap/SkillmapApi/Data/Seeder.cs
@@ -0,0 +1,61 @@
+﻿using System.Net.NetworkInformation;
+using Microsoft.AspNetCore.Identity;
+using SkillmapLib1.Models;
+
+namespace SkillmapApi.Data
+{
+    public class Seeder
+    {
+        public static async Task Seed(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager, DataContext dataContext)
+        {
+            if (!dataContext.Roles.Any())
+            {
+                await AddRoleAsync(roleManager, dataContext);
+            }
+            if (!dataContext.Users.Any())
+            {
+                await AddUserAsync(userManager, dataContext);
+            }
+            //if (dataContext.ResourcesItems.Any()) 
+            //{
+            //    await AddResourcesAsync(dataContext);
+            //}
+        }
+
+        //private static async Task AddResourcesAsync(DataContext dataContext)
+        //{
+        //    ResourcesItem item = new ResourcesItem
+        //    {
+        //        Title = "Item1",
+        //        Description = "Descrpcion item 1",
+        //    };
+        //}
+
+        private static async Task AddUserAsync(UserManager<IdentityUser> userManager, DataContext dataContext)
+        {
+            User user = new User
+            {
+                Name = "Admin",
+                Ap = "1",
+                Am = "2",
+                Email = "admin@mail.com",
+                UserName = "admin@mail.com",
+            };
+
+            var result = await userManager.CreateAsync(user, "123456");
+
+            if (result.Succeeded) 
+            {
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+
+        private static async Task AddRoleAsync(RoleManager<IdentityRole> roleManager, DataContext dataContext)
+        {
+            if(!await roleManager.RoleExistsAsync("Admin"))
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+        }
+    }
+}
--- Skillmap/SkillmapApi/Program.cs
+++ Skillmap/SkillmapApi/Program.cs
@@ -1,5 +1,7 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using SkillmapApi.Data;
 using SkillmapLib1.Models;
 
@@ -10,6 +12,22 @@ builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(builder.
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
 builder.Services.AddIdentityApiEndpoints<User>().AddRoles<IdentityRole>().AddEntityFrameworkStores<DataContext>();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("Version 1", new OpenApiInfo
+    {
+        Title = "Skillmap Api",
+        Description = "Skillmap Api",
+        Version = "v1",
+    });
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorizarion",
+        Type = SecuritySchemeType.ApiKey,
+    });
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -21,12 +39,23 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.MapIdentityApi<User>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scoped = app.Services.CreateScope())
+{
+    var serviceProvider = scoped.ServiceProvider;
+    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var dataContext = serviceProvider.GetRequiredService<DataContext>();
+}
 
 app.Run();
